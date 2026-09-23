a3e7ddb1d211c9c210ca40b2b02d9e99c5a4bb30
niko12315 <1592114890@qq.com>
2026-09-23 13:04:10 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lawquiz-v1';
+const CACHE = 'lawquiz-v2';
 const ASSETS = ['./', './index.html', './manifest.json',
                 './icon-180.png', './icon-192.png', './icon-512.png'];
 
